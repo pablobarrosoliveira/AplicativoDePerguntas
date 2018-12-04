cfdd20841c4a06f8c2c44dbc7bafed2c0d6ece98
Pablo Barros de Oliveira <pablo.barros.oliveira@everis.com>
2018-12-04 16:58:05 -0200

    Implementação do FireBase, Correção para reaparecer SplashScreen

--- app/src/main/java/br/org/appdeperguntas/aplicativodeperguntas/HomeActivity.java
+++ app/src/main/java/br/org/appdeperguntas/aplicativodeperguntas/HomeActivity.java
@@ -1,12 +1,11 @@
 package br.org.appdeperguntas.aplicativodeperguntas;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
--- app/src/main/java/br/org/appdeperguntas/aplicativodeperguntas/SplashScreenActivity.java
+++ app/src/main/java/br/org/appdeperguntas/aplicativodeperguntas/SplashScreenActivity.java
@@ -14,7 +14,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, LoginScreenActivity.class);
                 startActivity(intent);
                 finish();
             }
